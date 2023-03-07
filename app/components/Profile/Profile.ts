b2d4562ea26dc05750b8cbe393ccd519fc3fcc46
Santiago <santipr2004@gmail.com>
2023-03-07 13:19:04 -0500

    feat:Profile card

---
+++ app/components/Profile/Profile.ts
@@ -0,0 +1,57 @@
+export enum Attribute {
+    "name" = "name",
+    "email" = "email",
+    "city" = "city",
+    "nameCompany" = "nameCompany",
+}
+
+class MyProfile extends HTMLElement {
+    name?: string;
+    email?: string;
+    city?: string;
+    nameCompany?: string;
+    
+    static get observedAttributes() {
+        const attrs: Record<Attribute, null> = {
+            city: null,
+            email: null,
+            name: null,
+            nameCompany: null,
+        };
+        return Object.keys(attrs);
+    }
+    
+    constructor() {
+        super();
+        this.attachShadow({ mode: "open" });
+    }
+    
+    connectedCallback() {
+        this.render();
+    }
+    
+    attributeChangedCallback(
+        propName: Attribute,
+        _: string | undefined,
+        newValue: string | undefined
+        ) { 
+            this.render();
+        }
+        
+        render() {
+            if (this.shadowRoot) {
+                this.shadowRoot.innerHTML = `
+                <link rel="stylesheet" href="./app/components/profile/profile.css">
+                <section>
+                <h2>${this.name}</h2>
+                <p>${this.email}</p>
+                <p>${this.city}</p>
+                <p>${this.nameCompany}</p>
+                </section>
+                `;
+            }
+        }
+    }
+    
+customElements.define("my-profile", MyProfile);
+export default MyProfile;
---
+++ public/app/components/Profile/Profile.js
@@ -0,0 +1,43 @@
+export var Attribute;
+(function (Attribute) {
+    Attribute["name"] = "name";
+    Attribute["email"] = "email";
+    Attribute["city"] = "city";
+    Attribute["nameCompany"] = "nameCompany";
+})(Attribute || (Attribute = {}));
+class MyProfile extends HTMLElement {
+    static get observedAttributes() {
+        const attrs = {
+            city: null,
+            email: null,
+            name: null,
+            nameCompany: null,
+        };
+        return Object.keys(attrs);
+    }
+    constructor() {
+        super();
+        this.attachShadow({ mode: "open" });
+    }
+    connectedCallback() {
+        this.render();
+    }
+    attributeChangedCallback(propName, _, newValue) {
+        this.render();
+    }
+    render() {
+        if (this.shadowRoot) {
+            this.shadowRoot.innerHTML = `
+                <link rel="stylesheet" href="./app/components/profile/profile.css">
+                <section>
+                <h2>${this.name}</h2>
+                <p>${this.email}</p>
+                <p>${this.city}</p>
+                <p>${this.nameCompany}</p>
+                </section>
+                `;
+        }
+    }
+}
+customElements.define("my-profile", MyProfile);
+export default MyProfile;
